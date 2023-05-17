32213ac0c73fcd7b02e5c1915d2bb13d84ee534c
James Montemagno <james.montemagno@gmail.com>
2023-05-17 08:41:24 -0700

    Add project files.

---
+++ AwesomeConstructors/Program.cs
@@ -0,0 +1,31 @@
+﻿
+var logger = new Logger();
+
+var james = new Person("James", "Montemagno", logger);
+
+james.PrintName();
+
+
+public class Person(string firstName, string lastName, ILogger logger)
+{
+    public string FullName => $"{firstName} {lastName}";
+
+    public void PrintName()
+    {
+        logger.Log(FullName);
+    }
+
+}
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class Logger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
